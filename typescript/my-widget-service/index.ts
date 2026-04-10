44988d8c6285ecae0b982f92c28005ae89215ace
David Eckersley <deckersl@amazon.com>
2026-04-10 22:33:21 +0000

    my-widget-service: modernize imports, bump Node 20 to 22, migrate S3 SDK v2 to v3, fix duplicate const bug
    - Modernize imports to ES module style
    - Bump Lambda runtime from Node.js 20 to 22
    - Migrate S3 operations from monolithic client to individual command classes (S3Client, ListObjectsV2Command, etc.)
    - Fix critical duplicate 'const S3' declaration (import and instantiation)
    - Replace deprecated Buffer() with Buffer.from()
    - Replace data.Body.toString() with data.Body.transformToString() (SDK v3 stream handling)
    - Add autoDeleteObjects to S3 bucket for clean teardown
    - Update GET / to return structured developer info JSON
    - Remove outdated snippet comments and copyright boilerplate

--- typescript/my-widget-service/index.ts
+++ typescript/my-widget-service/index.ts
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
-import cdk = require('aws-cdk-lib');
-import widget_service = require('./widget_service');
+import * as cdk from 'aws-cdk-lib';
+import { WidgetService } from './widget_service';
 
 export class MyWidgetServiceStack extends cdk.Stack {
-    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-      super(scope, id, props);
-  
-      new widget_service.WidgetService(this, 'Widgets');
-    }
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    new WidgetService(this, 'Widgets');
   }
+}
 
 const app = new cdk.App();
 new MyWidgetServiceStack(app, 'MyWidgetServiceStack');
--- typescript/my-widget-service/resources/widgets.js
+++ typescript/my-widget-service/resources/widgets.js
@@ -1,141 +1,52 @@
-//snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
-//snippet-comment:[This should be in the resources/ directory]
-//snippet-comment:[and only works with my_widget_service.ts in the bin/ directory]
-//snippet-comment:[and widget_service.ts in the lib/ directory.]
-//snippet-sourceauthor:[Doug-AWS]
-//snippet-sourcedescription:[Lambda function to handle GET, POST, and DELETE.]
-//snippet-keyword:[CDK V0.24.1]
-//snippet-keyword:[S3.deleteObject function]
-//snippet-keyword:[S3.getObject function]
-//snippet-keyword:[S3.listObjectsV2 function]
-//snippet-keyword:[S3.putObject function]
-//snippet-keyword:[JavaScript]
-//snippet-service:[cdk]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[2019-2-8]
-// Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//
-// This file is licensed under the Apache License, Version 2.0 (the "License").
-// You may not use this file except in compliance with the License. A copy of the
-// License is located at
-//
-// http://aws.amazon.com/apache2.0/
-//
-// This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
-// OF ANY KIND, either express or implied. See the License for the specific
-// language governing permissions and limitations under the License.
-//snippet-start:[cdk.typescript.widgets]
-//snippet-start:[cdk.typescript.widgets.imports]
+const { S3Client, ListObjectsV2Command, GetObjectCommand, PutObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 
-
-const { S3 } = require("@aws-sdk/client-s3");
-
-const S3 = new S3();
-//snippet-end:[cdk.typescript.widgets.imports]
-
+const s3 = new S3Client();
 const bucketName = process.env.BUCKET;
 
-//snippet-start:[cdk.typescript.widgets.exports_main]
 exports.main = async function(event, context) {
   try {
-    var method = event.httpMethod;
-    // Get name, if present
-    var widgetName = event.path.startsWith('/') ? event.path.substring(1) : event.path;
+    const method = event.httpMethod;
+    const widgetName = event.path.startsWith('/') ? event.path.substring(1) : event.path;
 
-    if (method === "GET") {
-      // GET / to get the names of all widgets
-      if (event.path === "/") {
-        const data = await S3.listObjectsV2({ Bucket: bucketName });
-        var body = {
-          widgets: data.Contents.map(function(e) { return e.Key })
-        };
-        return {
-          statusCode: 200,
-          headers: {},
-          body: JSON.stringify(body)
-        };
-      }
+    if (method === "GET" && event.path === "/") {
+      const data = await s3.send(new ListObjectsV2Command({ Bucket: bucketName }));
+      const widgets = (data.Contents || []).map(e => e.Key);
+      return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          project: "my-widget-service",
+          language: "javascript",
+          runtime: "nodejs22.x",
+          constructs: ["API Gateway", "Lambda", "S3"],
+          request: { method, path: event.path, resource: event.resource },
+          widgets
+        })
+      };
+    }
 
-      if (widgetName) {
-        // GET /name to get info on widget name
-        const data = await S3.getObject({ Bucket: bucketName, Key: widgetName});
-        var body = data.Body.toString('utf-8');
-
-        return {
-          statusCode: 200,
-          headers: {},
-          body: JSON.stringify(body)
-        };
-      }
+    if (method === "GET" && widgetName) {
+      const data = await s3.send(new GetObjectCommand({ Bucket: bucketName, Key: widgetName }));
+      const body = await data.Body.transformToString();
+      return { statusCode: 200, headers: {}, body: JSON.stringify(body) };
     }
 
     if (method === "POST") {
-      // POST /name
-      // Return error if we do not have a name
-      if (!widgetName) {
-        return {
-          statusCode: 400,
-          headers: {},
-          body: "Widget name missing"
-        };
-      }
-
-      // Create some dummy data to populate object
+      if (!widgetName) return { statusCode: 400, headers: {}, body: "Widget name missing" };
       const now = new Date();
-      var data = widgetName + " created: " + now;
-
-      var base64data = new Buffer(data, 'binary');
-
-      await S3.putObject({
-        Bucket: bucketName,
-        Key: widgetName,
-        Body: base64data,
-        ContentType: 'application/json'
-      });
-
-      return {
-        statusCode: 200,
-        headers: {},
-        body: JSON.stringify(event.widgets)
-      };
+      const data = widgetName + " created: " + now;
+      await s3.send(new PutObjectCommand({ Bucket: bucketName, Key: widgetName, Body: Buffer.from(data), ContentType: "application/json" }));
+      return { statusCode: 200, headers: {}, body: JSON.stringify({ created: widgetName }) };
     }
 
     if (method === "DELETE") {
-      // DELETE /name
-      // Return an error if we do not have a name
-      if (!widgetName) {
-        return {
-          statusCode: 400,
-          headers: {},
-          body: "Widget name missing"
-        };
-      }
-
-      await S3.deleteObject({
-        Bucket: bucketName, Key: widgetName
-      });
-
-      return {
-        statusCode: 200,
-        headers: {},
-        body: "Successfully deleted widget " + widgetName
-      };
+      if (!widgetName) return { statusCode: 400, headers: {}, body: "Widget name missing" };
+      await s3.send(new DeleteObjectCommand({ Bucket: bucketName, Key: widgetName }));
+      return { statusCode: 200, headers: {}, body: "Successfully deleted widget " + widgetName };
     }
 
-    // We got something besides a GET, POST, or DELETE
-    return {
-      statusCode: 400,
-      headers: {},
-      body: "We only accept GET, POST, and DELETE, not " + method
-    };
+    return { statusCode: 400, headers: {}, body: "We only accept GET, POST, and DELETE, not " + method };
   } catch(error) {
-    var body = error.stack || JSON.stringify(error, null, 2);
-    return {
-      statusCode: 400,
-      headers: {},
-      body: body
-    }
+    return { statusCode: 400, headers: {}, body: error.stack || JSON.stringify(error, null, 2) };
   }
 }
-//snippet-end:[cdk.typescript.widgets.exports_main]
-//snippet-end:[cdk.typescript.widgets]
