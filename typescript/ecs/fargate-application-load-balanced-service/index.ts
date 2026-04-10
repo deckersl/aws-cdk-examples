58ec4145b7e2c93d5d6f01c38f5b056c878faec4
David Eckersley <deckersl@amazon.com>
2026-04-10 22:34:16 +0000

    fargate-application-load-balanced-service: modernize imports, add ALB DNS output
    - Modernize imports to ES module style
    - Add CfnOutput for ALB DNS name (useful for testing)
    - Add publicLoadBalancer: true explicitly
    - Remove unnecessary comments

--- typescript/ecs/fargate-application-load-balanced-service/index.ts
+++ typescript/ecs/fargate-application-load-balanced-service/index.ts
@@ -1,29 +1,30 @@
-import ec2 = require('aws-cdk-lib/aws-ec2');
-import ecs = require('aws-cdk-lib/aws-ecs');
-import ecs_patterns = require('aws-cdk-lib/aws-ecs-patterns');
-import cdk = require('aws-cdk-lib');
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as cdk from 'aws-cdk-lib';
 
 class BonjourFargate extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create VPC and Fargate Cluster
-    // NOTE: Limit AZs to avoid reaching resource quotas
     const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 });
     const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
 
-    // Instantiate Fargate Service with just cluster and image
-    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "FargateService", {
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "FargateService", {
       cluster,
       taskImageOptions: {
         image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
       },
+      publicLoadBalancer: true,
+    });
+
+    new cdk.CfnOutput(this, 'ALBDnsName', {
+      value: fargateService.loadBalancer.loadBalancerDnsName,
+      description: 'ALB DNS Name',
     });
   }
 }
 
 const app = new cdk.App();
-
-new BonjourFargate(app, 'Bonjour');
-
+new BonjourFargate(app, 'BonjourFargate');
 app.synth();
